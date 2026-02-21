2f5aeeb78d4b849e9d75dd380ffb9b99db943e21
adalbertobertulino-sketch <adalberto.bertulino@souprof.al.gov.br>
2026-02-20 23:13:46 -0300

    Update auth.js

--- js/js/auth.js
+++ js/js/auth.js
@@ -1,1 +1,42 @@
+import { auth } from "./firebase.js";
+import { 
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
+// LOGIN
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, senha);
+      window.location.href = "dashboard.html";
+    } catch (error) {
+      alert("Erro no login: " + error.message);
+    }
+  });
+}
+
+// VERIFICA USUÁRIO LOGADO
+onAuthStateChanged(auth, (user) => {
+  if (!user && window.location.pathname.includes("dashboard.html")) {
+    window.location.href = "index.html";
+  }
+});
+
+// LOGOUT
+const btnLogout = document.getElementById("logout");
+
+if (btnLogout) {
+  btnLogout.addEventListener("click", async () => {
+    await signOut(auth);
+    window.location.href = "index.html";
+  });
+}
